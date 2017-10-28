829d65bcc2a57b1dba2eb5a747d95092417183a8
noknod <noknod@mail.ru>
2017-10-28 21:23:11 +0500

    count/01_m.py 1

---
+++ count/01_m.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+
+import os
+import sys
+import re
+
+# 196.223.28.31 - - [16/Nov/2015:00:00:00 +0400] "GET /photo/manage.cgi HTTP/1.1" 200 0 "-" "Mozilla/6.66"
+
+
+TEMPLATE = """
+#!/usr/bin/env bash
+
+OUT_DIR=out
+NUM_REDUCERS=1 # > 0 to run the Reduce phase
+CONFIG="--config /home/agorokhov/conf.empty"
+
+hdfs dfs -rm -r -skipTrash out
+
+yarn jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
+    -D mapreduce.job.name="Uniq users step1" \
+    -D mapreduce.job.reduces=$NUM_REDUCERS \
+    -files metric01_1_mapper.py,metric01_1_reducer.py \
+    -mapper "./metric01_1_mapper.py" \
+    -reducer "./metric01_1_reducer.py" \
+    -input hdfs://{0} \
+    -output out
+"""
+
+ALL_FILES_FILE = '..prepare/data/all_dirs.txt'
+
+DONE_FILES_FILE = './data/m1_files_done.txt'
+
+
+def read_dirs_file(file_path):
+    answer = []
+    with open(file_path, 'r') as infile:
+        for line in infile.readlines():
+            line = line.strip()
+            if len(line) != 0:
+                answer.append(line.strip())
+    return set(answer)
+
+
+def main():
+    all_files = sored(list(read_dirs_file(ALL_FILES_FILE)))
+    done_files = read_dirs_file(DONE_FILES_FILE)
+    print 'All {0}, done {1} files'.format(len(all_files), len(done_files))
+
+    for file_path in all_files:
+        if file_path not in done_files:
+            print(file_path)
+            command = TEMPLATE.format(file_path)
+            result_code = int(os.system(command))
+            if result_code != 0:
+                print '\n\n*********\n\nERROR\n\n*********\n\n'
+                break
+            print '\ncomputed\n'
+
+            dir_path = file_path.split('/')[-2]
+            command = 'hdfs dfs -cp out/part-00000 hw1/metrics/{0}/m1_1_2.txt'.format(dir_path)
+            result_command = int(os.system(command))
+            if result_command != 0:
+                print '\n\n++++++++++\n\nERROR\n\n++++++++++\n\n'
+                break
+            add_dir_to_file(file_path, DONE_FILES_FILE)
+        break
+
+
+if __name__ == '__main__':
+    main()
