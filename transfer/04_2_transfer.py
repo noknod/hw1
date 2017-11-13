b5ae3be4a35f691b225ac4164b901826d29e5acc
noknod <noknod@mail.ru>
2017-11-13 20:27:37 +0300

    m4_2 2017-09-01 - 2017-11-12

---
+++ transfer/04_2_transfer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os
+
+
+ALL_FILES_FILE = '../count/data/m4_2_files_done.txt'
+
+DONE_FILES_FILE = './data/m4_2_files_done.txt'
+
+
+def read_dirs_file(file_path):
+    answer = []
+    with open(file_path, 'r') as infile:
+        for line in infile.readlines():
+            line = line.strip()
+            if len(line) != 0:
+                answer.append(line.strip())
+    return set(answer)
+
+
+def add_dir_to_file(current_dir, file_path):
+    with open(file_path, 'a') as outfile:
+        outfile.write(current_dir)
+        outfile.write('\n')
+
+
+def main():
+    all_files = sorted(list(read_dirs_file(ALL_FILES_FILE)))
+    done_files = read_dirs_file(DONE_FILES_FILE)
+    print 'All {0}, done {1} files'.format(len(all_files), len(done_files))
+
+    for file_path in all_files:
+        if file_path not in done_files:
+            #if file_path.split('/')[-2] < '2017-10-29':
+            #if file_path.split('/')[-2] in ('2017-10-02', '2017-10-03', '2017-10-04'):
+            #    continue
+            print(file_path)
+            file_date = file_path.split('/')[-2]
+            command = 'hdfs dfs -get hw1/metrics/{0}/m4_2.txt ../metrics/{0}/'.format(file_date)
+            result_command = int(os.system(command))
+            if result_command != 0:
+                print '\n\n++++++++++\n\nERROR\n\n++++++++++\n\n'
+                break
+            add_dir_to_file(file_path, DONE_FILES_FILE)
+        #break
+
+
+if __name__ == '__main__':
+    main()
