11413896076a29293be22d3b0285712b1b9441ba
yurij.klyushkin <yurij.klyushkin@pressindex.ru>
2017-12-09 20:30:25 +0500

    a

--- server.cmd
+++ server.cmd
@@ -1,3 +1,7 @@
 http://127.0.0.1:26273/api/hw3
 
 http://127.0.0.1:26273/api/hw3?start_date=2016-12-01&end_date=2016-12-01&profile_id=id10001&user_ip=192.168.0.190
+
+
+
+http://127.0.0.1:21523/api/hw3?start_date=2017-12-01&end_date=2017-12-01&profile_id=id10000&user_ip=192.168.0.190
